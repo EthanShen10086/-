81bb6122f5ad91a6287014eb118c65d3ff33e541
EthanShen <ethanshen4work@gmail.com>
2025-03-05 20:54:33 +0800

    feat: add 题目

--- 0.1+0.2/index.js
+++ 0.1+0.2/index.js
@@ -1,10 +1,7 @@
 function add(a, b) {
-	const factor = Math.pow(
-		10,
-		Math.max(
-			(String(a).split('.')[1] || '').length,
-			(String(b).split('.')[1] || '').length
-		)
-	);
+	// 获取小数部分
+	const aLength = (String(a).split('.')[1] || '').length;
+	const bLength = (String(b).split('.')[1] || '').length;
+	const factor = Math.pow(10, Math.max(aLength, bLength));
 	return (a * factor + b * factor) / factor;
 }
